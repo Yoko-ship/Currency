372c7394ba24f4b221a1838337344d91df04dfcf
User <yuko.brandon13@gmail.com>
2025-07-21 14:07:12 +0500

    refactored

--- something.py
+++ something.py
@@ -3,40 +3,46 @@ from dotenv import load_dotenv
 import os
 import tkinter as tk
 from tkinter import ttk
-load_dotenv()
 
+#* Загрузка
+load_dotenv()
 apiKey = os.environ.get("APIKEY")
 client = currencyapicom.Client(apiKey)
 
-def getCurrencyRate():
+def get_currency_rate():
     baseCurrency = baseCurrencyEntry.get().upper()
     currency = currencyEntry.get().upper()
+    if not baseCurrency or not currency:
+        rate_result_label.config(text="Пожалуста заполните все поля",fg="red")
+        return
     try:
         result = client.latest(baseCurrency,currencies=[currency])
         value = result['data'][currency]["value"]
-        if(value):
-            tk.Label(currencyRate,text=value,font=("Helteciva",14)).pack(pady=5)
+        rate_result_label.config(text=value)
     except Exception:
-        tk.Label(currencyRate,text="Пожалуста заполните все поля",fg="red",font=("Helvetica",14)).pack(pady=5)
+        rate_result_label.config(text="Произошла ошибка при получении курса",fg="red")
 
 
 
 def convertCurrencyInto():
     baseCurrency = baseCurrencyConvertEntry.get().upper()
-    try:
-        sum = int(sumEntry.get())
-    except ValueError:
-        tk.Label(currencyConvert,text="Сумма принимает только число").pack(pady=5)
+    sum_entry = sumEntry.get()
+    convert_result_rate.config(text="Сумма принимает только число!",fg="red")
     currency = currencyConvertEntry.get().upper()
+    if not baseCurrency or not sum or not currency:
+        convert_result_rate.config(text="Пожалуста заполните все поля",fg="red")
+        return
     try:
-
+        amount = float(sum_entry)
         result = client.latest(baseCurrency,currencies=[currency])
         value = result['data'][currency]["value"]
-        finalResult = sum * value
-        tk.Label(currencyConvert,text=finalResult,font=("Helvetica",14)).pack(pady=5)
+        finalResult = round(amount * value,2)
+        convert_result_rate.config(text=finalResult,fg='green')
     except Exception:
-        tk.Label(currencyConvert,text="Пожалуста заполните все поля",fg="red",font=("Helvetica",14)).pack(pady=5)
+        convert_result_rate.config(text="Произошла ошибка при получении курса",fg="red")
 
+
+#* UI
 root = tk.Tk()
 root.geometry("400x400")
 root.title("Currency")
@@ -46,26 +52,32 @@ currencyRate = ttk.Frame(notebook)
 currencyConvert = ttk.Frame(notebook)
 notebook.add(currencyRate,text="Узнать курс")
 notebook.add(currencyConvert,text="Преобразовать валюту")
-
+FONT = ("Helvetica",14)
+padding = 5
 ##* Курс
-tk.Label(currencyRate,text="Базовая валюта",font=("Helvetica",14)).pack(pady=5)
-baseCurrencyEntry = tk.Entry(currencyRate,font=("Helvetica",14),width=30,bd=2,relief="solid")
-baseCurrencyEntry.pack(pady=5)
-tk.Label(currencyRate,text="Курс какой валюты хотите получить",font=("Helvetica",14)).pack(pady=5)
-currencyEntry = tk.Entry(currencyRate,font=("Helvetica",14),width=30,bd=2,relief="groove")
-currencyEntry.pack(pady=5)
-confirmButton = tk.Button(currencyRate,text="Подтвердить",fg="white",bg="green",font=("Helvetica", 12),command=getCurrencyRate).pack(pady=5)
 
+tk.Label(currencyRate,text="Базовая валюта",font=FONT).pack(pady=padding)
+baseCurrencyEntry = tk.Entry(currencyRate,font=FONT,width=30,bd=2,relief="solid")
+baseCurrencyEntry.pack(pady=padding)
+tk.Label(currencyRate,text="Курс какой валюты хотите получить",font=FONT).pack(pady=padding)
+currencyEntry = tk.Entry(currencyRate,font=FONT,width=30,bd=2,relief="groove")
+currencyEntry.pack(pady=padding)
+confirmButton = tk.Button(currencyRate,text="Подтвердить",fg="white",bg="green",font=FONT,command=get_currency_rate).pack(pady=padding)
+rate_result_label = tk.Label(currencyRate,text="",font=FONT)
+rate_result_label.pack(pady=padding)
 #* Преобразовать
-tk.Label(currencyConvert,text="Базовая валюта",font=("Helvetica",14)).pack(pady=5)
-baseCurrencyConvertEntry = tk.Entry(currencyConvert,font=("Helvetica",14),width=30,bd=2,relief="groove")
-baseCurrencyConvertEntry.pack(pady=5)
-tk.Label(currencyConvert,text="Сумма",font=("Helvetica",14)).pack(pady=5)
-sumEntry = tk.Entry(currencyConvert,font=("Helvetica",14),width=30,bd=2,relief="groove",validate="key",validatecommand=(currencyConvert.register(lambda s: s.isdigit() or s == ""),"%S"))
-sumEntry.pack(pady=5)
-tk.Label(currencyConvert,text="Валюта",font=("Helvetica",14)).pack(pady=5)
-currencyConvertEntry = tk.Entry(currencyConvert,font=("Helvetica",14),width=30,bd=2,relief="groove")
-currencyConvertEntry.pack(pady=5)
-confirmConvertButton = tk.Button(currencyConvert,text="Подтвердить",fg="white",bg="green",font=("Helvetica",14),command=convertCurrencyInto).pack(pady=5)
+
+tk.Label(currencyConvert,text="Базовая валюта",font=FONT).pack(pady=padding)
+baseCurrencyConvertEntry = tk.Entry(currencyConvert,font=FONT,width=30,bd=2,relief="groove")
+baseCurrencyConvertEntry.pack(pady=padding)
+tk.Label(currencyConvert,text="Сумма",font=FONT).pack(pady=padding)
+sumEntry = tk.Entry(currencyConvert,font=FONT,width=30,bd=2,relief="groove",validate="key",validatecommand=(currencyConvert.register(lambda s: s.isdigit() or s == ""),"%S"))
+sumEntry.pack(pady=padding)
+tk.Label(currencyConvert,text="Валюта",font=FONT).pack(pady=padding)
+currencyConvertEntry = tk.Entry(currencyConvert,font=FONT,width=30,bd=2,relief="groove")
+currencyConvertEntry.pack(pady=padding)
+confirmConvertButton = tk.Button(currencyConvert,text="Подтвердить",fg="white",bg="green",font=FONT,command=convertCurrencyInto).pack(pady=padding)
+convert_result_rate = tk.Label(currencyConvert,text="",font=FONT)
+convert_result_rate.pack(pady=padding)
 root.mainloop()
 
